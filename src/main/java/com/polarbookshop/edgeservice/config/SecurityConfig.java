d8fd83bf11ea7d283373905423326a3de817dfba
Gompangii <gompangii@gmail.com>
2025-02-27 14:27:15 +0900

    SecurityConfig 클래스 오류 수정

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
@@ -19,9 +21,14 @@ import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
 //@Configuration
-@EnableWebFluxSecurity
-//@Configuration(proxyBeanMethods = false)
+//@EnableWebFluxSecurity
+@Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
+
+  @Bean
+  ServerOAuth2AuthorizedClientRepository authorizedClientRepository() {
+    return new WebSessionServerOAuth2AuthorizedClientRepository();
+  }
 
   @Bean
   SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository) {
