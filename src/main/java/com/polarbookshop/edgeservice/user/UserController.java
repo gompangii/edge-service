7451ff342e5a9c1f5556c8860c074cb12d26434c
Gompangii <gompangii@gmail.com>
2025-03-06 16:47:51 +0900

    13.1.1 스프링 부트를 사용한 로깅

--- src/main/java/com/polarbookshop/edgeservice/user/UserController.java
+++ src/main/java/com/polarbookshop/edgeservice/user/UserController.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.edgeservice.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +13,11 @@ import java.util.List;
 @RestController
 public class UserController {
 
+  private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
   @GetMapping("user")
   public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
+    log.info("Fetching information about the currently authenticated user");
     var user = new User(
       oidcUser.getPreferredUsername(),
       oidcUser.getGivenName(),
